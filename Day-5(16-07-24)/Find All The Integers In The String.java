d11ce6a99a75957a4e9e7e80e3331b69e97f7752
Mohammad Arman <141633522+mdarman4002@users.noreply.github.com>
2024-07-18 20:36:40 +0530

    Create Find All The Integers In The String.java
    Find All The Integers In The String
    Medium
    
    Description:
    Given a string A, consisting of comma-separated positive integers.
    
    Extract the given integers from the string and return an integer array consisting of the integers present in the string.
    
    Input Format:
    String with integers.
    
    Output Format:
    Array of all the integers in the string.
    
    Constraints:
    1 <= Array length <= 105
    
    Public Test Cases:
    Test Case 1:
    Input:
    1,2,3,4,5
    
    Output:
    [1,2,3,4,5]
    
    Explanation:
    Integers in the string are 1,2,3,4 and 5.
    
    Test Case 2:
    Input:
    12,34,56,1
    
    Output:
    [12,34,56,1]
    
    Explanation:
    Integers in string are 12,34,56 and 1.
    
    Tags:
    StringMathematical coding problems

---
+++ Day-5(16-07-24)/Find All The Integers In The String.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        String[] strArr = input.split(",");
+        int[] intArr = new int[strArr.length];
+
+        for (int i = 0; i < strArr.length; i++) {
+            intArr[i] = Integer.parseInt(strArr[i]);
+        }
+
+        System.out.println(Arrays.toString(intArr));
+    }
+}
