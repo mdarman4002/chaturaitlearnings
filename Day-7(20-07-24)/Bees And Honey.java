2d87706013fd765e24f133c3064ba6e69f5bc2e0
Mohammad Arman <141633522+mdarman4002@users.noreply.github.com>
2024-07-20 22:53:00 +0530

    Update Bees And Honey.java
    Bees And Honey
    Easy
    
    Description:
    There are n containers that store honey. Each container is given the amount of Honey stored in it in litres. And it is believed that 1 bee can make 10 ml of Honey (imaginary). Now there is a little girl who is eager to find out, how many bees make the honey. So help the little girl by returning the total number of bees.
    
    Input Format:
    n- number of containers
    
    arr[i] - space-separated array integers representing the honey in each container in litres
    
    Output Format:
    x - total number of bees
    
    Constraints:
    1<=n<=105
    
    1<= arr[i] <= 104
    
    Public Test Cases:
    Test Case 1:
    Input:
    3
    
    1 3 2
    
    Output:
    600
    
    Explanation:
    1st container - 1 lt = 1000ml = 100 bees
    
    2nd container - 3 lt = 3000ml = 300 bees
    
    3rd container - 2 lt - 2000ml = 200 bees
    
    total bees = 100+300+200=600

--- Day-7(20-07-24)/Bees And Honey.java
+++ Day-7(20-07-24)/Bees And Honey.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        long totalBees = 0;
+        long totalBees = 0;  
         for (int i = 0; i < n; i++) {
             int honeyInLitres = scanner.nextInt();
             totalBees += honeyInLitres * 100; // 1 liter = 1000 ml, 1 bee makes 10 ml honey
