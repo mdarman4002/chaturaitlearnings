a847b259675a43379bf23d1695969a6772df6425
Mohammad Arman <141633522+mdarman4002@users.noreply.github.com>
2024-08-09 14:51:37 +0530

    Create Code-5 , Building A Longest Palindrome.java
    Building A Longest Palindrome
    easy
    Description:
    
    Given a string str containing both lowercase and uppercase letters, your task is to find the length of the longest palindrome that can be constructed from the letters in the string. Please note that the letters are case-sensitive.
    
    Input Format:
    
    The input is provided as a single line that contains a string, which is referred to as 'str'.
    
    Output Format:
    
    Return the length of the longest palindrome that can be formed using a given string.
    
    Constraints:
    
    1 <= s.length <= 109
    
    Public Test Cases:
    Test Case 1
    Input:
    abccccdd
    
    Output:
    7
    
    Explanation:
    Self Explanatory
    
    Tags:
    
    String
    counting
    Greedy Algorithms

---
+++ The Grand Final Test Batch J-4/Code-5 , Building A Longest Palindrome.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        scanner.close();
+
+        System.out.println(longestPalindrome(str));
+    }
+
+    private static int longestPalindrome(String str) {
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+
+        int length = 0;
+        boolean oddFound = false;
+
+        for (int count : charCount.values()) {
+            if (count % 2 == 0) {
+                length += count;
+            } else {
+                length += count - 1;
+                oddFound = true;
+            }
+        }
+
+        if (oddFound) {
+            length += 1;
+        }
+
+        return length;
+    }
+}
